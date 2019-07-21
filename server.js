3995752337b4f71d0feeca721064d0b208111d44
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-07-21 13:17:30 -0700

    Adding connection to Heroku for MongoDB within server.js

--- server.js
+++ server.js
@@ -10,3 +10,8 @@ let app = express();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
+
+//connecting to MongoDB for Heroku
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);

97f88b2330a4aedefa09c84a351015f0753e4371
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-07-20 20:35:46 -0700

    Adding port, new instance of express, parse requesting and making public a static folder within server.js

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ var express = require("express");
 var mongoose = require("mongoose");
 var cheerio = require("cheerio");
 var axios = require("axios");
+
+const port = 8080;
+
+let app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("public"));

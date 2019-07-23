83fab43ad8a2e22d344e588f20161e96582e078c
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-07-22 21:49:35 -0700

    Adding scraping code within the axios call in server.js

--- server.js
+++ server.js
@@ -1,125 +1,116 @@
 var express = require("express");
-var logger = require("morgan");
 var mongoose = require("mongoose");
-
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
-var axios = require("axios");
 var cheerio = require("cheerio");
+var axios = require("axios");
 
-// Require all models
-var db = require("./models");
+const PORT = 8080;
 
-var PORT = 3000;
-
-// Initialize Express
 var app = express();
 
-// Configure middleware
-
-// Use morgan logger for logging requests
-app.use(logger("dev"));
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-// Make public a static folder
 app.use(express.static("public"));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var db = require("./models");
 
 //connecting to MongoDB for Heroku
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
-// Routes
 
-// A GET route for scraping the echoJS website
+app.get("/", function(req, res) {
+  db.GameObject.find({})
+    .then(function(dbGameObject) {
+      // If we were able to successfully find Articles, send them back to the client
+      var stuff = {
+        gameobject: dbGameObject
+      };
+      res.render("index", stuff);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+})
+
 app.get("/scrape", function(req, res) {
-  // First, we grab the body of the html with axios
-  axios.get("https://old.reddit.com/").then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    axios.get("https://www.greenmangaming.com/pc-games/").then(function (response) {
+
+    // Load the HTML into cheerio and save it to a variable
+    // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
     var $ = cheerio.load(response.data);
 
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
-      // Save an empty result object
+    console.log($);
+
+    // Select each element in the HTML body from which you want information.
+    // NOTE: Cheerio selectors function similarly to jQuery's selectors,
+    // but be sure to visit the package's npm page to see how it works
+    $("p.prod-name").each(function (i, element) {
       var result = {};
 
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
+      // Save these results in an object that we'll push into the results array we defined earlier
+      result.title = $(element)
+      .children("a").text().split("\n")[1];
+      result.image = $(element).parent().parent().parent().children("div.col-xs-4").children("div.media-object").children().children().attr("src");
+      result.link = "https://www.greenmangaming.com/pc-games" + $(element)
+      .children("a")
+      .attr("href");
 
-      // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
-        .then(function(dbArticle) {
-          // View the added result in the console
-          console.log(dbArticle);
+      // db.scrapedData.save(result, function (error, saved) {
+      //   if (error) {
+      //     console.log(error);
+      //   }
+      // });
+      db.GameObject.create(result)
+        .then(function(dbGameObject) {
+          console.log(dbGameObject);
         })
         .catch(function(err) {
-          // If an error occurred, log it
           console.log(err);
         });
     });
-
-    // Send a message to the client
-    res.send("Scrape Complete");
+    res.send("Scrape Complete!");
   });
 });
 
-// Route for getting all Articles from the db
-app.get("/articles", function(req, res) {
-  // Grab every document in the Articles collection
-  db.Article.find({})
-    .then(function(dbArticle) {
-      // If we were able to successfully find Articles, send them back to the client
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
+app.get("/gameobjects", function(req, res) {
+  db.GameObject.find({})
+  .then(function(dbGameObject) {
+    res.json(dbGameObject)
+  })
+  .catch(function(err) {
+    res.json(err);
+  });
 });
 
-// Route for grabbing a specific Article by id, populate it with it's note
-app.get("/articles/:id", function(req, res) {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-  db.Article.findOne({ _id: req.params.id })
-    // ..and populate all of the notes associated with it
+app.get("/gameobjects/:id", function(req, res) {
+  db.GameObject.findOne({_id: req.params.id})
     .populate("note")
-    .then(function(dbArticle) {
-      // If we were able to successfully find an Article with the given id, send it back to the client
-      res.json(dbArticle);
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
+    .then(function(dbGameObject) {
+      res.json(dbGameObject);
+    }).catch(function(err){
       res.json(err);
     });
 });
 
-// Route for saving/updating an Article's associated Note
-app.post("/articles/:id", function(req, res) {
-  // Create a new note and pass the req.body to the entry
+app.post("/gameobjects/:id", function(req, res) {
   db.Note.create(req.body)
     .then(function(dbNote) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-      return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      return db.GameObject.findOneAndUpdate({_id: req.params.id}, {note: dbNote._id}, { new: true});
     })
-    .then(function(dbArticle) {
-      // If we were able to successfully update an Article, send it back to the client
-      res.json(dbArticle);
+    .then(function(dbGameObject) {
+      res.json(dbGameObject);
     })
     .catch(function(err) {
-      // If an error occurred, send it to the client
       res.json(err);
     });
 });
 
-// Start the server
 app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
+    console.log("App running on port " + PORT + "!");
 });

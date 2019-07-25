69a1a91c9b03ae7d11c553f9325779453d46dc63
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-07-24 17:42:26 -0700

    Changing mispelling of mongooose to mongoose within server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ if (process.env.MONGODB_URI) {
   mongoose.connect(process.env.MONGODB_URI);
 }
 else {
-  mongooose.connect(databaseUrl);
+  mongoose.connect(databaseUrl);
 };
 
 app.get("/", function(req, res) {
